9b4c3c76b065419dad86303754ab6afe34694182
vafare <vafare92@gmail.com>
2020-11-16 04:46:01 +0330

    implement add section design

--- simple_todo_list/src/Components/Container/index.tsx
+++ simple_todo_list/src/Components/Container/index.tsx
@@ -1,55 +1,63 @@
-import React from "react"
-import ItemList from "../List/index"
-
+import React ,{useState} from "react";
+import ItemList from "../List/index";
 
 const Container = () => {
-    const readyItems = [
-        {
-            title: "Buy bread",
-            compeleted: false,
-          },
-          {
-            title: "Buy pepsi",
-            compeleted: false,
-          },
-          {
-            title: "Buy Vegtables",
-            compeleted: false,
-          },
-          {
-            title: "Buy Meat",
-            compeleted: false,
-          },
-        ];
+  const [subject, setSubject] = useState("");
 
-        const doneItems = [
-          {
-              title: "go to the office",
-              compeleted: true,
-            },
-            {
-              title: "Pay to friend",
-              compeleted: true,
-            },
-            
-          ];
-    
+  const readyItems = [
+    {
+      title: "Buy bread",
+      compeleted: false,
+    },
+    {
+      title: "Buy pepsi",
+      compeleted: false,
+    },
+    {
+      title: "Buy Vegtables",
+      compeleted: false,
+    },
+    {
+      title: "Buy Meat",
+      compeleted: false,
+    },
+  ];
+
+  const doneItems = [
+    {
+      title: "go to the office",
+      compeleted: true,
+    },
+    {
+      title: "Pay to friend",
+      compeleted: true,
+    },
+  ];
+
   return (
     <div className="rootContainer">
       <div className="topSection">
-       <h1 >ToDo List:</h1>
-       
+        <h1>ToDo List:</h1>
+        <div className="addView">
+          <input
+            className="inputFields"
+            placeholder="Subject"
+            onChange={(e) => setSubject(e.target.value)}
+          />
+          <button
+            className="btn"
+            
+          >
+            {" "}
+            +
+          </button>
+        </div>
       </div>
       <div className="parrentView">
-          <ItemList
-            items ={readyItems}
-            done={false}
-          />
-          <hr className="seprateLine"/>
-          <ItemList items={doneItems} done={true} />
-        </div>
-      
-      
+        <ItemList items={readyItems} done={false} />
+        <hr className="seprateLine" />
+        <ItemList items={doneItems} done={true} />
+      </div>
     </div>
   );
 };
