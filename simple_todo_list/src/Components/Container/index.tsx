3f91819196afed1b4194c1b52fa7800a6ad5d88c
vafare <vafare92@gmail.com>
2020-11-16 05:16:02 +0330

    implement delete function

--- simple_todo_list/src/Components/Container/index.tsx
+++ simple_todo_list/src/Components/Container/index.tsx
@@ -1,11 +1,19 @@
-import React ,{useState} from "react";
+import React ,{useState , useCallback} from "react";
 import store from "../../redux/store/store"
+import { todoItem } from "../../types/types";
 import ItemList from "../List/index";
-
+import {
+  deleteItem,
+} from "../../redux/actions";
 const Container = () => {
   const [subject, setSubject] = useState("");
   const [ready, setReady] = useState(store.getState().itemsReady);
   const [complete, setComplete] = useState(store.getState().itemsDone);
+
+  const deleteItemList = useCallback((item: todoItem) => {
+    store.dispatch(deleteItem(item));
+    setReady(store.getState().itemsReady);
+  }, []);
 
 
   return (
@@ -28,7 +36,7 @@ const Container = () => {
         </div>
       </div>
       <div className="parrentView">
-        <ItemList items={ready} done={false} />
+        <ItemList items={ready} deleteItems={deleteItemList} done={false} />
         <hr className="seprateLine" />
         <ItemList items={complete} done={true} />
       </div>
--- simple_todo_list/src/Components/List/index.tsx
+++ simple_todo_list/src/Components/List/index.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { todoItem } from "../../types/types";
 
-const ItemList = (props: any) => {
-  const items = useState(props.items);
+const ItemList = (props: {
+  items: Array<todoItem>;
+  makeCompelete?: any;
+  done: boolean;
+  deleteItems?: any;
+}) => {
+  const items: todoItem[] = props.items;
   return (
     <ul className="rootItemViews">
       {items.map((item: any) => (
@@ -24,7 +30,7 @@ const ItemList = (props: any) => {
               <button
                 className="deleteBtn"
                 onClick={() => {
-                  props.deleteItema(item);
+                  props.deleteItems(item);
                 }}
               >
                 <FontAwesomeIcon icon={faTimes} size={"sm"} color={"red"} />
