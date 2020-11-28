eb064da7b41766c9873d30cad8af98da46b4a7c2
vafare <vafare92@gmail.com>
2020-11-28 11:41:53 +0330

    Initialize packages

---
+++ simple_todo_list/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        0.5: "0.125rem",
+      },
+      width: {
+        "1/7": "14.2857143%",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
